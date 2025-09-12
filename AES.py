d15d086984205b9b91cb98407e4f80d6a2856857
Rodinez <mngtandrade@gmail.com>
2025-09-12 10:51:36 -0300

    import do módulo typing

--- AES.py
+++ AES.py
@@ -25,12 +25,22 @@ def main():
             bytes_do_texto.append(0)
         blocos = [bytes_do_texto[i:i+16] for i in range(0, len(bytes_do_texto), 16)]
         
-        chaves_da_rodada = expande_chave(chave)
+        chaves_por_rodada = expande_chave(chave)
         blocos_cifrados = []
         
         for bloco in blocos:
             estado = [bloco[i:i+4] for i in range(0, 16, 4)]
-            estado = xor_com_chave(estado, chaves_da_rodada[0])
+            estado = xor_com_chave(estado, chaves_por_rodada[0])
+            for rodada in range(1,10):
+                estado = substitui_bytes(estado)
+                estado = desloca_linhas(estado)
+                estado = embaralha_colunas(estado)
+                estado = xor_com_chave(estado, chaves_por_rodada[rodada])
+            estado = substitui_bytes(estado)
+            estado = desloca_linhas(estado)
+            estado = xor_com_chave(estado, chaves_por_rodada[10])
+            
+        blocos_cifrados.append([estado[i][j] for i in range(4) for j in range(4)])
 
         print("\nFormato de saída:")
         print("1 - Hexadecimal")
