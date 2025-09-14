4a058906f423fc28cc8e4a6d8eb73f53aa73351a
Rodinez <mngtandrade@gmail.com>
2025-09-14 16:52:11 -0300

    comentários no código principal para auxiliar no entendimento

--- AES.py
+++ AES.py
@@ -16,32 +16,38 @@ def main():
         chave_input = input("Digite a chave: ")
 
         if formato_chave == "1":  
-            chave = list(chave_input.encode())
-            while len(chave) < 16:
-                chave.append(0)
-            chave = chave[:16]
+            chave = list(chave_input.encode()) # string -> hex
+            while len(chave) < 16: 
+                chave.append(0) # padding se necessário
+            chave = chave[:16] # 16 bytes para a chave
         else:  
-            chave = [int(chave_input[i:i+2], 16) for i in range(0, 32, 2)]
+            # 16 bytes para a chave 2 a 2 dígitos no formato hexadecimal
+            chave = [int(chave_input[i:i+2], 16) for i in range(0, 32, 2)] 
         
         print()
         mensagem = input("Digite a mensagem a ser cifrada (string): ")
         
-        bytes_do_texto = list(mensagem.encode())
-        while len(bytes_do_texto) % 16 != 0:
+        bytes_do_texto = list(mensagem.encode()) # string -> hex
+        while len(bytes_do_texto) % 16 != 0: 
             bytes_do_texto.append(0)
-        blocos = [bytes_do_texto[i:i+16] for i in range(0, len(bytes_do_texto), 16)]
+        # blocos de 16 bytes
+        blocos = [bytes_do_texto[i:i+16] for i in range(0, len(bytes_do_texto), 16)] 
                 
         chaves_por_rodada = expande_chave(chave)
         blocos_cifrados = []
         
         for bloco in blocos:
-            estado = [[bloco[linha + 4*coluna] for coluna in range(4)] for linha in range(4)]
-            estado = xor_com_chave(estado, chaves_por_rodada[0])
-            for rodada in range(1,10):
+            # transposta do bloco
+            estado = [[bloco[linha + 4*coluna] for coluna in range(4)] for linha in range(4)] 
+            # 1ª rodada da cifragem
+            estado = xor_com_chave(estado, chaves_por_rodada[0]) 
+            # rodadas 2 a 10 da cifragem
+            for rodada in range(1,10): 
                 estado = substitui_bytes(estado)
                 estado = desloca_linhas(estado)
                 estado = embaralha_colunas(estado)
                 estado = xor_com_chave(estado, chaves_por_rodada[rodada])
+            # última rodada da cifragem (11ª)
             estado = substitui_bytes(estado)
             estado = desloca_linhas(estado)
             estado = xor_com_chave(estado, chaves_por_rodada[10])
@@ -70,11 +76,12 @@ def main():
         print()
 
         if formato_chave == "1":  
-            chave = list(chave_input.encode())
-            while len(chave) < 16:
-                chave.append(0)
-            chave = chave[:16]
-        else:  
+            chave = list(chave_input.encode()) # string -> hex
+            while len(chave) < 16: 
+                chave.append(0)  # padding se necessário
+            chave = chave[:16] # 16 bytes para a chave
+        else:
+            # 16 bytes para a chave 2 a 2 dígitos no formato hexadecimal
             chave = [int(chave_input[i:i+2], 16) for i in range(0, 32, 2)]
         
         print("Formato da mensagem cifrada:")
@@ -89,24 +96,30 @@ def main():
         bytes_cifrados = []
 
         if formato_msg == "1": 
-            bytes_cifrados = [int(mensagem_cifrada[i:i+2], 16) for i in range(0, len(mensagem_cifrada), 2)]
+            # divisão de 2 em 2 no formato hexadecimal
+            bytes_cifrados = [int(mensagem_cifrada[i:i+2], 16) for i in range(0, len(mensagem_cifrada), 2)] 
         else:  
-            for byte in mensagem_cifrada.split():
+            for byte in mensagem_cifrada.split(): # formato 117 119 10 2 32 ..
                 bytes_cifrados.append(int(byte))
         
         
+        # blocos de 16 bytes
         blocos = [bytes_cifrados[i:i+16] for i in range(0, len(bytes_cifrados), 16)]
         chaves_por_rodada = expande_chave(chave)
         blocos_decifrados = []
         
         for bloco in blocos:
+            # transposta do bloco
             estado = [[bloco[linha + 4*coluna] for coluna in range(4)] for linha in range(4)]
+            # 1ª rodada da descifragem
             estado = xor_com_chave(estado, chaves_por_rodada[10])
+            # rodadas 2 a 10 da descifragem
             for rodada in range(9, 0, -1):
                 estado = arruma_linhas(estado)
                 estado = reverte_bytes(estado)
                 estado = xor_com_chave(estado, chaves_por_rodada[rodada])
                 estado = desembaralha_colunas(estado)
+            # última rodada da descifragem (11ª)
             estado = arruma_linhas(estado)
             estado = reverte_bytes(estado)
             estado = xor_com_chave(estado, chaves_por_rodada[0])
@@ -123,7 +136,7 @@ def main():
 
         if saida_opcao == "1":
             try:
-                mensagem = bytes(mensagem_bytes).decode()
+                mensagem = bytes(mensagem_bytes).decode() # hex -> string
                 print("Mensagem decifrada (string):", mensagem)
             except UnicodeDecodeError:
                 print("utf-8 não conseguiu decodificar algum byte")
